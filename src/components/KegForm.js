cc17adfa4b496af8cd08ef986e8c6ace3c613e00
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-07-02 18:43:51 -0700

    add modular KegForm

---
+++ src/components/KegForm.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function KegFrom(props) {
+  return (
+    <React.Fragment>
+      <form onSubmit={props.submitHandler}>
+        <input
+          type='text'
+          name='name'
+          placeholder='Potion Name' />
+        <input
+          type='text'
+          name='brewer'
+          placeholder='Brewer of Potion' />
+        <input
+          type='number'
+          name='price'
+          placeholder='Cost per draught' />
+        <textarea
+          name='description'
+          placeholder='Describe effects of yon brew' />
+        <button type='submit'>{props.buttonText}</button>
+      </form>
+    </React.Fragment>
+  )
+}
+
+KegForm.propTypes = {
+  submitHandler: PropTypes.func,
+  buttonText: PropTypes.string
+}
+
+export default KegForm;
