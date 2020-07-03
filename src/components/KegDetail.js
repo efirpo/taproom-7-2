66fa5557b8f005f3b97a7005ad455728a8adae47
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-07-02 20:26:18 -0700

    add edit keg function(buggy)

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import KegForm from './KegForm';
 
-function KegDetail() {
+function KegDetail(props) {
+
+  return (
+    <React.Fragment>
+      <h2>{props.keg.name}</h2>
+      <p>Brewed by: {props.keg.brewer}</p>
+      <p><em>{props.keg.description}</em></p>
+      <p>Price per draught: <strong>{props.keg.price} gp</strong></p>
+      <br /><br /><hr />
+      <button onClick={props.onClickingEdit}>Edit this Brew</button>
+      {props.editKegForm}
+
+    </React.Fragment>
+  )
 
 }
-
+KegDetail.propTypes = {
+  keg: PropTypes.object,
+  onClickingEdit: PropTypes.func,
+  onClickingDelete: PropTypes.func,
+  editFormVisible: PropTypes.bool,
+}
 export default KegDetail;
--- src/components/TaproomControl.js
+++ src/components/TaproomControl.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AddKeg from './AddKeg';
 import KegList from './KegList';
 import KegDetail from './KegDetail';
+import KegForm from './KegForm';
 
 
 class TaproomControl extends React.Component {
@@ -26,18 +27,44 @@ class TaproomControl extends React.Component {
     })
   }
 
+  handleClickingShowEdit = () => {
+    this.setState({
+      editFormVisible: !this.state.editFormVisible
+    })
+  }
+
+  handleClickingEditSubmit = (keg) => {
+    const newMasterKegList = this.state.masterKegList.map((obj, index) => (obj.id === keg.id ? Object.assign({}, this.state.masterKegList[index], { keg }) : console.log("Error, fool!")))
+    this.setState({
+      masterKegList: newMasterKegList
+    })
+  }
+
+
   render() {
     let kegSelected = null;
 
     if (this.state.selectedKeg !== null) {
-      kegSelected = <KegDetail keg={this.state.selectedKeg} />
+      if (this.state.editFormVisible) {
+        kegSelected = <KegDetail keg={this.state.selectedKeg} onClickingEdit={this.handleClickingShowEdit} editKegForm={<KegForm submitHandler={this.handleClickingEditSubmit} buttonText="Edit this Keg" />} />
+      } else {
+        kegSelected = <KegDetail keg={this.state.selectedKeg} onClickingEdit={this.handleClickingShowEdit} />
+      }
+
     }
+
 
     return (
       <React.Fragment>
-        <AddKeg onNewKegCreation={this.handleAddingNewKegToStock} />
-        <KegList kegList={this.state.masterKegList} onKegSelect={this.handleSelectingKeg} />
-        {kegSelected}
+        <div id='full-page'>
+          <div id='add-and-list'>
+            <AddKeg onNewKegCreation={this.handleAddingNewKegToStock} />
+            <KegList kegList={this.state.masterKegList} onKegSelect={this.handleSelectingKeg} />
+          </div>
+          <div id='details-and-edit'>
+            {kegSelected}
+          </div>
+        </div>
       </React.Fragment>
     )
   }
