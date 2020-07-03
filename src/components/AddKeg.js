d8f6edd1b3a4686f82ee6b16621f0ed6a41288e5
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-07-02 18:53:37 -0700

    add AddKeg component

---
+++ src/components/AddKeg.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { v4 } from 'uuid';
+import KegForm from './KegForm';
+
+function AddKeg(props) {
+
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    props.onNewKegCreation({ name: event.target.name.value, brewer: event.target.brewer.value, price: event.target.price.value, description: event.target.description.value, id: v4() })
+  }
+  return (
+    <React.Fragment>
+      <KegForm submitHandler={handleNewKegFormSubmission} buttonText="Add Keg to Stock" />
+    </React.Fragment>
+  )
+}
+
+AddKeg.propTypes = {
+  onNewKegCreation: PropTypes.func
+}
+
+export default AddKeg;
